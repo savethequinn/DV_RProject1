5a7bad52dceabbc8c72e21169ddec010d9309e5d
Justin Owens <justinowens@utexas.edu>
2015-02-04 16:47:08 -0600

    maybe final

--- 02 Visualizations/VisualOrders.R
+++ 02 Visualizations/VisualOrders.R
@@ -1,5 +1,2 @@
-<<<<<<< HEAD
-ggplot(dfOrders,aes( x = ORDER_ID, y = as.Date(SHIPPED_DATE, "%Y-%m-%d"))) + geom_point() + ggtitle("Orders")
-=======
 ggplot(dfOrders,aes( x = ORDER_ID, y = as.Date(SHIPPED_DATE, "%Y-%m-%d"))) + geom_point(na.rm = TRUE) + ggtitle("Orders")
->>>>>>> 32171c8f3e11e48be1096a2c5490951d5079b881
+
